bd914f8804d0371bfd771c54a0fffdf828cd6743
Mahmoud Shihab <mahmoud.a.shihab@gmail.com>
2024-09-19 12:05:46 -0600

    create empty routes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import "dotenv/config";
 import Users from "./mongodb/Users.js";
 import Sheets from "./mongodb/Sheets.js";
-import "dotenv/config";
+import sheetRouter from "./routes/sheets.js";
+import sheetRouter from "./routes/users.js";
 
 const app = express();
 const { PORT, CORS_ORIGIN, DB_USERNAME, DB_PASSWORD } = process.env;
@@ -14,13 +16,15 @@ app.use(express.json());
 app.use(cors(CORS_ORIGIN));
 app.use(express.static("public"));
 
-app.route("/").get(async (_req,res)=>{
-    // const sheet = await Sheets.find()
-    const sheet = await Sheets.findById("66eb3bb9faf44d1cb74b41ac").exec()
-    res.json(sheet);
-    // const users = await Users.findById("66e1c70685f3d12a4a71028c").exec()
-    // res.json(users);
-})
+// app.route("/").get(async (_req,res)=>{
+//     // const sheet = await Sheets.find()
+//     const sheet = await Sheets.findById("66eb3bb9faf44d1cb74b41ac").exec()
+//     res.json(sheet);
+//     // const users = await Users.findById("66e1c70685f3d12a4a71028c").exec()
+//     // res.json(users);
+// })
+app.use("/api/v1/sheets",sheetRouter); 
+app.use("/api/v1/users",sheetRouter); 
 
 app.listen(PORT, () => {
     console.log(`App running on PORT: http://localhost:${PORT}/`);
